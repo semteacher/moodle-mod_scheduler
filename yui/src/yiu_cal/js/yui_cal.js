db62c62b5b61f29f0ecb226e45ec8f1b4a7ff0f7
semteacher <semteacher@gmail.com>
2013-12-31 08:45:47 +0200

    test yui failed
    Signed-off-by: semteacher <semteacher@gmail.com>

--- yui/src/yiu_cal/js/yui_cal.js
+++ yui/src/yiu_cal/js/yui_cal.js
@@ -1,13 +1,20 @@
-﻿YUI().use("moodle-mod_scheduler-yui_cal", function(Y)
+﻿YUI().use("yui2-calendar", function(Y)
 {
+//  var YAHOO = Y.YUI2;
+//  var cal = new YAHOO.widget.Calendar("calContainer");
+//  cal.render();
+M.mod_scheduler = M.mod_scheduler || {};
+M.mod_scheduler.init_yui_cal = function() {
   var YAHOO = Y.YUI2;
   var cal = new YAHOO.widget.Calendar("calContainer");
   cal.render();
-  
-  M.mod_scheduler = M.mod_scheduler || {};
-  M.mod_scheduler.yui_cal = {
-    init: function() {
-
-    }
-  };  
+ //   Y.delegate('click', function(e) {
+        // Alert users when they've clicked on some fruit to tell them the obvious.
+ //       alert("You clicked on some fruit");
+ 
+        // Add a border to the fruit so we can see that it was selected.
+ //       e.setStyle('border', '1px solid black');
+  //  }, Y.config.doc, '.scheduler');
+};
 });
+
--- yui/yui_cal.js
+++ yui/yui_cal.js
@@ -1,6 +1,21 @@
-﻿YUI().use("yui2-calendar", function(Y)
+﻿YUI().use("moodle-mod_scheduler-yui_cal", function(Y)
 {
+//  var YAHOO = Y.YUI2;
+//  var cal = new YAHOO.widget.Calendar("calContainer");
+//  cal.render();
+M.mod_scheduler = M.mod_scheduler || {};
+M.mod_scheduler.yui_cal = {
+    init: function() {
   var YAHOO = Y.YUI2;
   var cal = new YAHOO.widget.Calendar("calContainer");
   cal.render();
+ //   Y.delegate('click', function(e) {
+        // Alert users when they've clicked on some fruit to tell them the obvious.
+ //       alert("You clicked on some fruit");
+ 
+        // Add a border to the fruit so we can see that it was selected.
+ //       e.setStyle('border', '1px solid black');
+  //  }, Y.config.doc, '.scheduler');
+  }
+};
 });
