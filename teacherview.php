ea048fee771a6893654e7df22920d1acf47eb42d
semteacher <semteacher@gmail.com>
2014-04-19 20:24:04 +0300

    yui_cal2.js
    -errors not present
    -still not working
    
    Signed-off-by: semteacher <semteacher@gmail.com>

--- teacherview.php
+++ teacherview.php
@@ -275,8 +275,8 @@ if ($action == 'addaperiodsession') {
 //        echo '</script>';
 ////////////     form_init_mycalendar_js();
     $config=array();
-    $function ='M.mod_scheduler.init_yui_cal';
-    $module   = 'moodle-mod_scheduler-yui_cal';
+    $function ='M.mod_scheduler.init_yui_cal2';
+    $module   = 'moodle-mod_scheduler-yui_cal2';
     //    $function = 'M.mod_scheduler.yui_cal1.init_date_selectors';
     $PAGE->requires->yui_module($module, $function, $config);
 
---
+++ yui/src/yiu_cal/js/yui_cal1.js
@@ -0,0 +1,30 @@
+﻿YUI().use("calendar", function(Y)
+{
+
+var calendar = new Y.Calendar({
+          contentBox: "#mycalendar",
+          height:'200px',
+          width:'600px',
+          showPrevMonth: true,
+          showNextMonth: true,
+          date: new Date(2010,11,1)}).render();
+
+//  var YAHOO = Y.YUI2;
+//  var cal = new YAHOO.widget.Calendar("calContainer");
+//  cal.render();
+//M.mod_scheduler = M.mod_scheduler || {};
+//M.mod_scheduler.yui_cal1 = {
+//    init_date_selectors: function() {
+//  var YAHOO = Y.YUI2;
+//  var cal = new YAHOO.widget.Calendar("calContainer");
+//  cal.render();
+ //   Y.delegate('click', function(e) {
+        // Alert users when they've clicked on some fruit to tell them the obvious.
+ //       alert("You clicked on some fruit");
+ 
+        // Add a border to the fruit so we can see that it was selected.
+ //       e.setStyle('border', '1px solid black');
+  //  }, Y.config.doc, '.scheduler');
+//  }
+//};
+});
